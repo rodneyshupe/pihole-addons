e63270722c33b923016e79547d76a2541604882d
rodneyshupe <rodney@shupe.ca>
2023-02-06 02:19:22 -0800

    Add reboot option

--- setup.sh
+++ setup.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 
+function confirm() {
+    local prompt="$1"
+    local default=${2:-Y}
+    local exit_on_no=${3:-false}
+
+    if [[ "$default" == "Y" ]]; then
+        choice="Y/n"
+    elif [[ "$default" == "N" ]]; then
+        choice="y/N"
+    else
+        choice="y/n"
+    fi
+    echo -n "$prompt [$choice] " >&2
+    read answer
+
+    [[ "$answer" == "" ]] && answer="$default"
+    
+    case "$answer" in
+        Y|y)
+            return 0
+            ;;
+        N|n)
+            if $exit_on_no; then
+                echo "Exit!" >&2
+                exit 1
+            else
+                return 1
+            fi
+            ;;
+        *)
+            echo "Invalid response." >&2
+            return confirm "$prompt" "$default" "$exit_on_no"
+            ;;
+    esac
+}
+
 function run_script() {
     local source=$1
     local script=${2:-$HOME/.scripts/"$(basename "${source%.*}")"_"$(basename "$(dirname "$source")")".sh}
@@ -17,3 +53,8 @@ run_script https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/spee
 
 # TODO: node-exporter
 # TODO: OpenCanary
+
+echo "It is recommended that you do a full reboot."
+if confirm "Do you want to Reboot now " "N"; then
+    sudo shutdown -r now
+fi
