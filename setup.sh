ddaef38a3dd6417fffe8ac679fe77955e5f754af
rodneyshupe <rodney@shupe.ca>
2023-02-06 00:24:30 -0800

    Fixed script running code

--- setup.sh
+++ setup.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/env bash
 
-function confirm() {
-    local prompt="$1"
-    local exit_on_no=${2:-false}
-    read -p "$prompt [Y/n] " answer
-    case "$answer" in
-        Y|y|"")
-            return 0
-            ;;
-        N|n)
-            if $exit_on_no; then
-                echo "Exit!"
-                exit 1
-            else
-                return 1
-            fi
-            ;;
-        *)
-            echo "Invalid response."
-            return confirm "$prompt" "$exit_on_no"
-            ;;
-    esac
+function run_script() {
+    local source=$1
+    local script=${:-$HOME/.scripts/"$(basename "${source%.*}")"_"$(basename "$(dirname "$source")")".sh}
+
+    mkdir -p "$(dirname "$scripts")"
+    curl -sSL $source --output $script
+    chmod +x "$script"
+    "$script"
 }
 
-curl -sSL https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/PADD/install.sh | sudo bash
-curl -sSL https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/nginx-proxy-manager/install.sh | sudo bash
-curl -sSL https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/heimdall/install.sh | sudo bash
-curl -sSL https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/speedtest/install.sh | sudo bash
+run_script https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/PADD/install.sh
+run_script https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/nginx-proxy-manager/install.sh
+run_script https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/heimdall/install.sh
+run_script https://raw.githubusercontent.com/rodneyshupe/pihole-addons/main/speedtest/install.sh
+
 # TODO: node-exporter
 # TODO: OpenCanary
